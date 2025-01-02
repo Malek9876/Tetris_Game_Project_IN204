1b4789026a045473081f57f127a5d9cca327923a
bouazizelyes <bouazizelyes999@gmail.com>
2025-01-02 20:49:40 +0100

    blabla

---
+++ 3d-tetris/build/CMakeFiles/3D_Tetris.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/elyes/Documents/GitHub/Tetris_Game_Project_IN204/3d-tetris/src/main.cpp" "CMakeFiles/3D_Tetris.dir/src/main.cpp.o" "gcc" "CMakeFiles/3D_Tetris.dir/src/main.cpp.o.d"
+  "/home/elyes/Documents/GitHub/Tetris_Game_Project_IN204/3d-tetris/src/utils/utils.cpp" "CMakeFiles/3D_Tetris.dir/src/utils/utils.cpp.o" "gcc" "CMakeFiles/3D_Tetris.dir/src/utils/utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 3d-tetris/build/CMakeFiles/3D_Tetris.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for 3D_Tetris.
+# This may be replaced when dependencies are built.
